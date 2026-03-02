b2e0398902e3136e5ae2eac72356b73dcbb78db6
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-02 18:08:50 +0530

    Add implementation to count even and odd numbers in an array

---
+++ tcs_practice_question/even.java
@@ -0,0 +1,19 @@
+// count even and odd number in an array.
+class even{
+    public static void main(String[]args){
+        int [] arr = {1,2,3,4,53,44,54,55,34};
+        int evenCount = 0;
+        int oddCount = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]%2==0){
+                evenCount++;
+            }
+            else{
+                oddCount++;
+            }   
+
+        }
+        System.out.println("Even Count: " + evenCount);
+        System.out.println("Odd Count: " + oddCount);
+    }
+}
