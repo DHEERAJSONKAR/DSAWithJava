acc09ceb17aa3a7a436728e9f532721a3ad5506b
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-12 19:28:23 +0530

    Add test case for duplicate element detection in array

--- tcs_practice_question/duplicateelement.java
+++ tcs_practice_question/duplicateelement.java
@@ -2,7 +2,7 @@
 import java.util.HashSet;
 class duplicateelement{
     public static void main(String[] args){
-        int [] arr = {1,2,3,4,5,6,7};
+        int [] arr = {1,2,3,3,4,5,6,7};
         HashSet<Integer> set = new HashSet<>();
         boolean hasDuplicate = false;
         for(int num: arr){
