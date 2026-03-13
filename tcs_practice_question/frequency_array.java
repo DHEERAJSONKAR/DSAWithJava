056741e2067c005d53319b098e9237e2913252f4
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-13 23:35:24 +0530

    Add implementation to find frequency of elements in an array

---
+++ tcs_practice_question/frequency_array.java
@@ -0,0 +1,26 @@
+// find a frequency in a array.
+import java.util.HashMap;
+
+public class frequency_array {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,2,3,1,4,2};
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : arr){
+
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            }
+            else{
+                map.put(num, 1);
+            }
+
+        }
+
+        for(Integer key : map.keySet()){
+            System.out.println(key + " -> " + map.get(key));
+        }
+    }
+}
