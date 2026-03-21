a01114c0eb975675b04222b207df5cd4315a2c19
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-21 23:21:52 +0530

    Add implementation for rotating an array to the left by k elements

--- tcs_practice_question/rotate_array.java
+++ tcs_practice_question/rotate_array.java
@@ -10,8 +10,8 @@ class rotate_array{
         }
     }
     public static void main(String[] args){
-        int[] arr = {12,3,4,10,45};
-        int k = 3;
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
         int n = arr.length;
         k= k%n;
         reverse(arr, 0, n-1);
@@ -19,6 +19,6 @@ class rotate_array{
         reverse(arr, k, n-1);
     
      for(int num : arr)
-    System.out.print(num + " ");
+    System.out.println(num + " ");
     }
 }
