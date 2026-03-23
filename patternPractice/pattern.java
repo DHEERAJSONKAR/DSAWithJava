cb300cfe50a62105244d1ca8303c8157190fc5b2
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-23 12:22:15 +0530

    Add implementations for right-angle number triangle and alphabetical triangle patterns

--- patternPractice/pattern.java
+++ patternPractice/pattern.java
@@ -68,11 +68,35 @@
 
 
 // 6. Inverted rigth - angle triangle
+// class pattern{
+//   public static void main(String[] args){
+//     for(int i =5; i>=1; i--){
+//       for(int j = 1; j<=i; j++){
+//         System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+// 7.right-angle number triangle where each row repeats.
+
+// class pattern{
+//   public static void main(String[] args){
+//     for(int i =1; i<=5; i++){
+//       for(int j= 1; j<=i; j++){
+//         System.out.print(i);
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+// 8.alphabetical triangle
 class pattern{
   public static void main(String[] args){
-    for(int i =5; i>=1; i--){
-      for(int j = 1; j<=i; j++){
-        System.out.print("*");
+    for(int i=1; i<=4; i++){
+      for(int j =1; j<=i; j++){
+        System.out.print((char)(64+i));
       }
       System.out.println();
     }
