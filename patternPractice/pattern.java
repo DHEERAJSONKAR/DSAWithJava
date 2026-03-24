40c5ff8613f643dbca1792429192dae244160cea
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-24 23:58:27 +0530

    Fix output method in right-angle number triangle pattern implementation

--- patternPractice/pattern.java
+++ patternPractice/pattern.java
@@ -96,7 +96,7 @@ class pattern{
   public static void main(String[] args){
     for(int i=1; i<=4; i++){
       for(int j =1; j<=i; j++){
-        System.out.print((char)(64+i));
+        System.out.println((char)(64+i));
       }
       System.out.println();
     }
