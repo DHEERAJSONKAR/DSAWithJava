70538baf9db3fedf8a36f0d1597b5bca5d001798
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-29 18:10:41 +0530

    Add implementation for zig-zag and hollow square patterns in pyramid.java

--- patternPractice/pyramid.java
+++ patternPractice/pyramid.java
@@ -174,13 +174,32 @@
 // }
 
 // Zig-Zag pattern
+// public class pyramid {
+//     public static void main(String[] args) {
+
+//         for (int i = 1; i <= 3; i++) {
+//             for (int j = 1; j <= 9; j++) {
+
+//                 if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// hollow squar pattern
 public class pyramid {
     public static void main(String[] args) {
+        int n = 5;
 
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 9; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
 
-                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
+                if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
