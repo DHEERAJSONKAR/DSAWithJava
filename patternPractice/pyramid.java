df4ec652097ae17f3d5ca45b6b13635f08007cda
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-30 23:16:23 +0530

    Add implementation for hollow square pattern and Floyd's triangle in pyramid.java

--- patternPractice/pyramid.java
+++ patternPractice/pyramid.java
@@ -192,19 +192,37 @@
 // }
 
 // hollow squar pattern
+// public class pyramid {
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+
+//                 if (i == 1 || i == n || j == 1 || j == n) {
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// floy'd Trianlge
 public class pyramid {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
+        int num = 1;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
 
-                if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
             }
+
             System.out.println();
         }
     }
