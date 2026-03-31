cac81e0d5adadce3f7eafe49521053bca5a04901
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-03-31 23:34:02 +0530

    Refactor Floyd's triangle implementation to improve readability

--- patternPractice/pyramid.java
+++ patternPractice/pyramid.java
@@ -210,20 +210,20 @@
 //     }
 // }
 
-// floy'd Trianlge
-public class pyramid {
-    public static void main(String[] args) {
-        int n = 5;
-        int num = 1;
+// floy'd Trianlge pattern question
+// public class pyramid {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         int num = 1;
 
-        for (int i = 1; i <= n; i++) {
+//         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print(num + " ");
-                num++;
-            }
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(num + " ");
+//                 num++;
+//             }
 
-            System.out.println();
-        }
-    }
-}
+//             System.out.println();
+//         }
+//     }
+// }
